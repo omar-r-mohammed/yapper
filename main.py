a8e5103242c93ce638a674cefab88dcfc24a3453
Nabeel Ahmad <nabeelfaisalahmad@gmail.com>
2023-11-07 18:39:05 -0600

    Made changes to main.py

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ Creation date: 11/07/23
 Last modified: 11/07/23
 """
 
-from flask import Flask 
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'americaOnline1996!'
 socketio = SocketIO(app)
+
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message received!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app,debug=True)
